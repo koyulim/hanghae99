aebfc06d3e0661a5963d361a87a3a606f3512c1f
고유림 <89690794+koyulim@users.noreply.github.com>
2024-07-27 16:37:52 +0900

    240727

---
+++ 240727/src/App.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        String[][] case1 = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}}; // 5
+        int result = solution(case1);
+        
+        System.out.print(result + " ");
+    }
+
+    public static int solution(String[][] clothes) {
+
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        for (int i = 0; i < clothes.length; i++) {
+            if (map.containsKey(clothes[i][1])) {
+                map.replace(clothes[i][1], (map.get(clothes[i][1])) + 1);
+            } else {
+                map.put(clothes[i][1], 1);
+            }
+        }
+        
+        for (String key : map.keySet()) {
+            answer = ((map.get(key)) + 1) * answer;
+        }
+    
+        return answer - 1;
+    }
+}
