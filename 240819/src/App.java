14d7f121fc7cfb5f34829a4732221814e0293764
고유림 <89690794+koyulim@users.noreply.github.com>
2024-08-19 18:44:20 +0900

    240819

--- 240819/src/App.java
+++ 240819/src/App.java
@@ -1,5 +1,37 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] nums = {10,9,2,5,3,7,101,18};
+        int result = lengthOfLIS(nums);
+        System.out.println(result);
+    }
+
+    public static int lengthOfLIS(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        list.add(nums[0]);
+       
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > list.get(list.size()-1)) {
+                list.add(nums[i]);
+            } else {
+
+                int left = 0;
+                int right = list.size();
+                int target = nums[i];
+
+                while (left < right) {
+                    int mid = (left + right) / 2;
+                    if (list.get(mid) < target) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+                
+                list.set(left, nums[i]);
+            }
+        }
+        return list.size();
     }
 }
